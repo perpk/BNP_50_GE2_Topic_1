d62b044cf6515291eac0aca25360ac99a88b9bca
Konstantinos Perperidis <k.a.perperidis@gmail.com>
2023-11-16 10:28:08 +0200

    initial commit

---
+++ bnp50_ge2_1st_topic.m
@@ -0,0 +1,27 @@
+clear all;
+close all;
+clc;
+display('BNP 50 1st Topic - Epidemiological Model')
+
+beta = 0.5;
+lamda = 0.1;
+delta = 0.02;
+N = 1000;
+
+t = linspace(0, 100, 1001);
+
+v0 = [990;10;0;0];
+
+[t z] = ode45(@(t, z) epidemic_odefn(z, t, beta, lamda, delta, N), t, v0);
+
+figure(1);
+subplot(2, 1, 1);
+plot(t, z(:,1), 'r', t, z(:,2), 'g', t, z(:,3), 'b', t, z(:,4), 'black');
+legend('Susceptible', 'Infected', 'Recovered', 'Dead');
+xlabel('Time in Days')
+ylabel('Population')
+grid;
+
+subplot(2, 1, 2);
+plot3(v0, z(1), t);
+
