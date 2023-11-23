4921aa58984b6afcac942bc55d64e89a0fb7d448
Konstantinos Perperidis <k.a.perperidis@gmail.com>
2023-11-23 18:51:35 +0200

    Change the linspace for plotting the phase diagram

--- bnp50_ge2_lab.m
+++ bnp50_ge2_lab.m
@@ -13,9 +13,13 @@ t = linspace(0, 100, 1001);
 figure;
 hold on;
 grid on;
-for s = 990:100:2000
-    for i = 10:100:2000
-        for r = 0:100:2000
+xlabel('S');
+ylabel('I');
+zlabel('R');
+
+for s = 100:100:1500
+    for i = 10:100:1500
+        for r = 0:100:1500
             [t, sir] = ode45(@(t, sir) epidemic_odefn_phases(sir, t, beta, lamda, delta, N), t, [s, i, r]);
             plot3(sir(:,1), sir(:,2), sir(:,3));
         end % for r
