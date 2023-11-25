730c22549ad3b6b2b2cd30e7fbaff39c8978a086
Konstantinos Perperidis <k.a.perperidis@gmail.com>
2023-11-25 15:31:04 +0200

    Quiver3 vector field plot

--- bpn_50_ge2_quiver_mesh.m
+++ bpn_50_ge2_quiver_mesh.m
@@ -1,31 +1,18 @@
 clear all;
 close all;
 clc;
-
+disp('BNP 50 1st Topic - Quiver');
 beta = 0.5;
 lamda = 0.1;
 delta = 0.02;
-N = 1000;
-
-t = linspace(0, 100, 1001);
-v0 = [990;10;0;0];
-
-u = @(s,i,r) -beta.* s.* i.* 1/N;
-v = @(s,i,r) beta.* s.* i.* 1/N - lamda.* i - delta.* i;
-w = @(s,i,r) lamda.* i;
-
-x = linspace(0,100,20);
-y = linspace(0,100,20);
-z = linspace(0,100,20);
-
-[x y z] = meshgrid(x,y,z);
-
-U = u(x,y,z);
-V = v(x,y,z);
-W = w(x,y,z);
-
-quiver3(x,y,z,U,V,W);
+N = 1000;  
+v0 = [990;10;0];
+[s, i, r] = meshgrid(0:5:50, 0:5:50, 0:5:50);
+sdot=-beta.*s.*i.*1/N;
+idot=beta.*s.*i.*1/N-lamda.*i-delta.*i;
+rdot=lamda.*i;
+quiver3(s,i,r,sdot,idot,rdot);
+grid;
 xlabel('S');
-ylabel('R')
-zlabel('I')
-grid on;
+ylabel('I');
+zlabel('R');
